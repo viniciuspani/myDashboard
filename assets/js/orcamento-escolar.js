908c471a33ac80948bbe93598888e8dbfe639c7b
viniciuspani <viniciuspani@hotmail.com>
2019-10-29 17:14:48 -0300

    Jquery orcamento.js

--- assets/js/orcamento-escolar.js
+++ assets/js/orcamento-escolar.js
@@ -1,9 +1,23 @@
 const materialSelect = document.getElementById("materialSelect");
 
 
-var listaMaterial = ["Lápis", "Caneta", "Borracha"];
+var listaMaterial = ["Lápis", "Caneta", "Borracha", "Cola", "Tesoura", "Caderno", "Estojo", "Lápis de cor (12 und)"];
 
 listaMaterial.forEach((material) => {
-    option = new Option(material, material.toLowerCase());
-    materialSelect.options[materialSelect.options.length] = option;
-  });
+  option = new Option(material, material.toLowerCase());
+  materialSelect.options[materialSelect.options.length] = option;
+});
+
+
+var nomeAlunoMat = document.getElementById('nomeAlunoMat');
+var valorGastar = document.getElementById('valorGastar');
+
+
+function adicionarMaterial() {
+  $("nomeAlunoMat").on("focus", function () {
+    $("#adicionarDados").attr("disabled", true);
+  }
+  $("valorGastar").on("focus", function () {
+    $("adicionarDados").attr("disabled", true);
+  }
+  )}
